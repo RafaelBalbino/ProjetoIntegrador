93abd59c7bff2b293e64fe19c21b47c4842bd9ee
unknown <taisanjostata@gmail.com>
2022-06-06 09:56:46 -0300

    Adicionado a variavel tipo | @TaisAnjos

--- O Projeto/caiv/src/main/java/com/generation/caiv/model/Usuario.java
+++ O Projeto/caiv/src/main/java/com/generation/caiv/model/Usuario.java
@@ -37,6 +37,8 @@ public class Usuario {
 	
 	private String foto;
 	
+	private String tipo;
+	
 	@OneToMany (mappedBy = "usuario", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties ("usuario")
 	private List <Postagem> postagem;
@@ -87,5 +89,13 @@ public class Usuario {
 
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 }
